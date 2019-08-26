c6ca0acc610ee47d6c6f1803134d0d858354d71c
Roman <romariom4k@ya.ru>
2019-08-26 19:21:44 +0300

    add reset function

--- main.js
+++ main.js
@@ -3,35 +3,56 @@
     let startButton = document.querySelector('.startGame');
     let gameField = document.querySelector('.gameField');
     let cells = gameField.querySelectorAll('.cell');
-    let crossesScore = document.querySelector('.crossesScore');
-    let noughtsScore = document.querySelector('.noughtsScore');
+    let crossesScoreSpan = document.querySelector('.crossesScore');
+    let noughtsScoreSpan = document.querySelector('.noughtsScore');
+    let crossesScore = 0;
+    let noughtsScore = 0;
     let resetButton = document.querySelector('.resetButton');
     let curentPlayer = 'X';
+
+
+    let startGame = function(){
+        gameField.classList.add('pointer');
+        startButton.disabled = true;
+        resetButton.disabled = false;
+        title.textContent = `Ход игрока ${curentPlayer}`;
+        gameField.addEventListener('click', gameTern);
+        resetButton.addEventListener('click', gameReset);
+    };
 
     let gameTern = function(evt){
         let cell = evt.target;
         if (!cell.textContent){
             cell.textContent = curentPlayer;
             if (curentPlayer === 'X'){
-                console.log(cell.classList);
                 cell.classList.add('cross');
-                curentPlayer = '0';
             } else {
                 cell.classList.add('nought');
-                curentPlayer = 'X';
             }
-        }
-    }
+        };
+        winCheck(curentPlayer);
+        curentPlayer = (curentPlayer === 'X') ? '0' : 'X';
+    };
 
-    let startGame = function(){
-        gameField.classList.add('pointer');
-        startButton.disabled = true;
-        resetButton.disabled = false;
-        title.textContent = `Ход игрока ${curentPlayer}`
-        gameField.addEventListener('click', gameTern);
+    let winCheck = function(){
+
+    };
+
+    let gameReset = function(){
+        gameField.classList.remove('pointer');
+        startButton.disabled = false;
+        resetButton.disabled = true;
+        title.textContent = `Игра "Крестики-нолики"`;
+        gameField.removeEventListener('click', gameTern);
+        resetButton.removeEventListener('click', gameReset);
+        cells.forEach(function(cell){
+            cell.textContent = '';
+            cell.classList.remove('cross');
+            cell.classList.remove('nought');
+        });
+        crossesScore = 0;
+        noughtsScore = 0;
     };
 
     startButton.addEventListener('click', startGame);
-
-    
 })()
